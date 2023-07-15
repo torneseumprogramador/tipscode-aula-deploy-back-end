36ea25fd8c4b1632e139b7fc8f1664b569ee7b99
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-07-15 11:57:51 -0300

    liberando cros

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors'); // Importe o módulo 'cors'
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors()); // Use o middleware 'cors' para habilitar o Cross-Domain
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
