673117f87fb416c3215d6a93cacc4cfa6864b506
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-07-15 13:15:10 -0300

    fix

--- entities/user.js
+++ entities/user.js
@@ -1,11 +1,11 @@
 class User {
     constructor(user) {
       if(user === null) user = {}
-      
-      this.id = user?.id;
-      this.name = user?.name;
-      this.phone = user?.phone;
-      this.obs = user?.obs;
+
+      this.id = user.id;
+      this.name = user.name;
+      this.phone = user.phone;
+      this.obs = user.obs;
     }
   }
   
